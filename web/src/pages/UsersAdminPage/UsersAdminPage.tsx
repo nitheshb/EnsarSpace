859b5c2dc2477afaba495d9a8af0a26c15725249
munivenkatesh99 <venkatesh@ensarsolutions.com>
2023-05-16 23:50:52 +0530

    modified the data

--- web/src/pages/UsersAdminPage/UsersAdminPage.tsx
+++ web/src/pages/UsersAdminPage/UsersAdminPage.tsx
@@ -30,19 +30,19 @@ const UsersAdminPage = () => {
 
 
         <SlimSideMenuBar
-              pgName={'hrModule'}
-              sourceLink={'hrModule'}
-              showSideView1={undefined}
-              setViewable={setViewable}
-              viewable={viewable}
-            />
+          pgName={'hrModule'}
+          sourceLink={'hrModule'}
+          showSideView1={undefined}
+          setViewable={setViewable}
+          viewable={viewable}
+        />
 
         <div className="flex flex-col flex-grow">
           {/* <HeadNavBar /> */}
           <HeadNavBar2
-              selModule ={selModule}
-              setSelModule={setSelModule}
-            />
+            selModule={selModule}
+            setSelModule={setSelModule}
+          />
           <div className="flex-grow p-6 overflow-auto  text-gray-700 bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200">
             <div className="flex items-center flex-shrink-0 h-16 px-0  pl-0  ">
               {/* <h1 className="text-lg font-medium">redefine.</h1> */}
@@ -94,6 +94,15 @@ const UsersAdminPage = () => {
               <>
                 <MyActivityHome source={'team'} />
               </>
+            )}
+            {viewable === 'Pay Slips' && (
+
+              <>
+
+                {/* <UserAccessTable /> */}
+
+              </>
+
             )}
 
             <SUserSignup
