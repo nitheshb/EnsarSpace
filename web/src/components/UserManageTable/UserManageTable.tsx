bfe402b5a11e3b27f899f91b579d6e1066a56cc7
Ooha12Yeturu <ooha@ensarsolutions.com>
2023-05-31 09:32:12 +0530

    updates

--- web/src/components/UserManageTable/UserManageTable.tsx
+++ web/src/components/UserManageTable/UserManageTable.tsx
@@ -1,3 +1,4 @@
+
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
@@ -61,14 +62,15 @@ const UserManageTable = ({ editEmployeeFun }) => {
           <section className="flex ml-auto mt-[18px]  bg-white  border-gray-100 py-4 md:py-7 px-4 md:px-8 xl:px-10">
             {[
               { label: 'All', val: 'all' },
-              { label: 'IT Employee', val: 'itemployee' },
-              { label: 'IT Desk', val: 'itdesk' },
+              { label: 'IT Employee', val: 'it employee' },
+              { label: 'IT Desk', val: 'it desk' },
               { label: 'HR', val: 'hr' },
               { label: 'Finance', val: 'finance' },
               { label: 'Project', val: 'project' },
               { label: 'Sales', val: 'sales' },
               { label: 'Learning', val: 'learning' },
               { label: 'Admin', val: 'admin' },
+
             ].map((dat, index) => (
               <a
                 key={index}
@@ -76,7 +78,7 @@ const UserManageTable = ({ editEmployeeFun }) => {
                 onClick={() => showOnlyDept(dat.val)}
               >
                 <div
-                  className={`py-2 px-8 rounded-full hover:text-indigo-700 hover:bg-indigo-100  ${
+                  className={`py-2 px-6 rounded-full hover:text-indigo-700 hover:bg-indigo-100  ${
                     selDept.includes(dat.val)
                       ? 'bg-indigo-100 text-indigo-700'
                       : 'text-gray-600'
