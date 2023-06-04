fa808635578c3246d6ae5e2a22b9d45af142b97a
munivenkatesh99 <venkatesh@ensarsolutions.com>
2023-06-04 12:50:15 +0530

    leaves

--- web/src/components/SUserSignupBody/SUserSignupBody.tsx
+++ web/src/components/SUserSignupBody/SUserSignupBody.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useEffect, useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import * as Yup from 'yup'
