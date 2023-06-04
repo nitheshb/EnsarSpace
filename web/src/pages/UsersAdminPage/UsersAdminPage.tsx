4f597d68511995c6cf06d9854f64c6a006f9755d
munivenkatesh99 <venkatesh@ensarsolutions.com>
2023-06-04 12:11:53 +0530

    creating a table to display leaves

--- web/src/pages/UsersAdminPage/UsersAdminPage.tsx
+++ web/src/pages/UsersAdminPage/UsersAdminPage.tsx
@@ -77,6 +77,7 @@ const UsersAdminPage = () => {
               )}
             </div>
 
+
             {viewable === 'User Management' && (
               <UserManageTable editEmployeeFun={editEmployeeFun} />
             )}
