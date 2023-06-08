a95d9328e309a9cd3aa49daca232584a38eace0b
Pavan644 <pavan.m@ensarsolutions.com>
2023-06-08 16:06:17 +0530

    Modified page

--- web/src/components/A_LearningModule/Attendance.tsx
+++ web/src/components/A_LearningModule/Attendance.tsx
@@ -1,29 +1,56 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Button, Card, CardContent, Table, TableBody, TableCell, TableHead, TableRow, TextField, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, Table, TableBody, TextField, TableCell, TableHead, TableRow, Typography } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import SearchIcon from '@mui/icons-material/Search';
 import EditIcon from '@mui/icons-material/Edit';
-import SaveIcon from '@mui/icons-material/Save';
-import { SearchIcon } from '@heroicons/react/outline';
+
+
+
 
 const AttendancePage = () => {
   const [checkInTime, setCheckInTime] = useState('');
   const [checkOutTime, setCheckOutTime] = useState('');
   const [attendanceData, setAttendanceData] = useState([]);
-  const [editMode, setEditMode] = useState(null);
-  const [editedCheckIn, setEditedCheckIn] = useState('');
-  const [editedCheckOut, setEditedCheckOut] = useState('');
+
+
   const [startMonth, setStartMonth] = useState('');
   const [endMonth, setEndMonth] = useState('');
 
+  const [editMode, setEditMode] = useState(null);
+  const [editedCheckIn, setEditedCheckIn] = useState('');
+  const [editedCheckOut, setEditedCheckOut] = useState('');
 
-  function getCurrentDate() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0');
-    const day = String(now.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  }
 
+
+  const handleEdit = (id) => {
+    const recordToEdit = attendanceData.find((record) => record.id === id);
+    setEditMode(id);
+    setEditedCheckIn(recordToEdit.checkIn);
+    setEditedCheckOut(recordToEdit.checkOut);
+  };
+
+  const handleSave = (id, editedCheckIn, editedCheckOut) => {
+    const updatedData = attendanceData.map((record) => {
+      if (record.id === id) {
+        const totalHours = calculateTotalHours(editedCheckIn, editedCheckOut);
+        return {
+          ...record,
+          checkIn: editedCheckIn,
+          checkOut: editedCheckOut,
+          totalHours: totalHours,
+        };
+      }
+      return record;
+    });
+
+    setAttendanceData(updatedData);
+    setEditMode(null);
+  };
+
+
+
+
+  // Load attendance data from localStorage on component mount
   useEffect(() => {
     const storedAttendanceData = localStorage.getItem('attendanceData');
     if (storedAttendanceData) {
@@ -31,6 +58,9 @@ const AttendancePage = () => {
     }
   }, []);
 
+
+
+  // Save attendance data to localStorage whenever it changes
   useEffect(() => {
     localStorage.setItem('attendanceData', JSON.stringify(attendanceData));
   }, [attendanceData]);
@@ -50,7 +80,7 @@ const AttendancePage = () => {
 
     const attendanceRecord = {
       id: new Date().getTime(),
-      date: new Date().toLocaleDateString(),
+      date: new Date().toLocaleDateString('en-GB', { day: 'numeric', month: 'short', year: 'numeric' }),
       checkIn: checkInTime,
       checkOut: checkOutTime,
       totalHours: totalHours,
@@ -68,181 +98,263 @@ const AttendancePage = () => {
   const calculateTotalHours = (checkIn, checkOut) => {
     const checkInTime = new Date(`2000/01/01 ${checkIn}`);
     const checkOutTime = new Date(`2000/01/01 ${checkOut}`);
-    const diffInMilliseconds = checkOutTime - checkInTime;
+    const diff = checkOutTime - checkInTime;
 
-    const hours = Math.floor(diffInMilliseconds / (1000 * 60 * 60));
-    const minutes = Math.floor((diffInMilliseconds / (1000 * 60)) % 60);
-    const seconds = Math.floor((diffInMilliseconds / 1000) % 60);
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const minutes = Math.floor((diff / (1000 * 60)) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
 
     return `${hours} hours ${minutes} minutes ${seconds} seconds`;
   };
 
-  const handleEdit = (id, checkIn, checkOut) => {
-    setEditMode(id);
-    setEditedCheckIn(checkIn);
-    setEditedCheckOut(checkOut);
-  };
-
-  const handleSave = (id) => {
-    const updatedData = attendanceData.map((record) => {
-      if (record.id === id) {
-        const totalHours = calculateTotalHours(editedCheckIn, editedCheckOut);
-        return {
-          ...record,
-          checkIn: editedCheckIn,
-          checkOut: editedCheckOut,
-          totalHours: totalHours,
-        };
-      }
-      return record;
-    });
-
-    setAttendanceData(updatedData);
-    setEditMode(null);
-  };
+  // const handleSearch = () => {
+  //   console.log('Searching...');
+  // };
 
   const handleSearch = () => {
-    console.log('Searching...');
+    const startDate = new Date(startMonth);
+    const endDate = new Date(endMonth);
+
+    const filteredData = attendanceData.filter((record) => {
+      const recordDate = new Date(record.date);
+      return recordDate >= startDate && recordDate <= endDate;
+    });
+
+    if (filteredData.length > 0) {
+      console.log('Filtered data:', filteredData);
+    } else {
+      console.log('No records found within the specified date range');
+    }
   };
 
-  const handleStartMonthChange = (e) => {
-    setStartMonth(e.target.value);
-    setEndMonth('');
-  };
 
-  // Function to handle changes in the End Date field
-  const handleEndMonthChange = (e) => {
-    setEndMonth(e.target.value);
+
+
+
+  useEffect(() => {
+    getCurrentMonthRange();
+  }, []);
+
+  const getCurrentMonthRange = () => {
+    const currentDate = new Date();
+    const startOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+    const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+
+    setStartMonth(startOfMonth.toISOString().slice(0, 10));
+    setEndMonth(endOfMonth.toISOString().slice(0, 10));
   };
 
   return (
-    <Card>
-      <Card sx={{ mt: 2 }}>
-      <CardContent sx={{ display: 'flex', justifyContent: 'space-between', border: 'none' }}>
-  <Box display="flex" alignItems="center" gap={2}>
-    <Typography variant="body1">
-      Current Date: {new Date().toLocaleDateString('en-GB', { day: 'numeric', month: 'short', year: 'numeric' })}
-    </Typography>
-  </Box>
-  <Box display="flex" alignItems="center" gap={2}>
-
-    <Button variant="contained" onClick={handleCheckIn}>
-      Check In
-    </Button>
-    <Button variant="contained" onClick={handleCheckOut} disabled={!checkInTime}>
-      Check Out
-    </Button>
-    <Button variant="contained" onClick={handleSubmit} disabled={!checkInTime || !checkOutTime}>
-      Submit
-    </Button>
-  </Box>
-</CardContent>
-      </Card>
-
-
+    <Box>
       <Card>
-      <CardContent sx={{ display: 'flex', justifyContent: 'flex-end', border: 'none' }}>
-        <TextField
-          label="Start Date"
-          type="date"
-          value={startMonth}
-          onChange={handleStartMonthChange}
-          sx={{ width: '150px', marginRight: '5px' }}
-        />
-        <TextField
-          label="End Date"
-          type="date"
-          value={endMonth}
-          onChange={handleEndMonthChange}
-          disabled={!startMonth}
-          sx={{ width: '150px', marginRight: '5px' }}
-        />
-        <Button variant="contained" onClick={handleSearch} disabled={!startMonth || !endMonth}>
-          <SearchIcon />
-        </Button>
-      </CardContent>
-    </Card>
+
+        <Card sx={{ mt: 2 }}>
+          <CardContent sx={{ display: 'flex', alignItems: 'center', gap: '90px', ml: 10, justifyContent: 'between' }}>
+            <Typography variant="body1">
+              Current Date: {new Date().toLocaleDateString('en-GB', { day: 'numeric', month: 'short', year: 'numeric' })}
+            </Typography>
+
+            <div>
+              {checkInTime && (
+                <Typography variant="body2" sx={{ marginBottom: '8px' }}>{checkInTime}</Typography>
+              )}
+
+              <Button variant="contained" onClick={handleCheckIn}>
+                Check In
+              </Button>
+
+            </div>
+
+
+            <div>
+              {checkOutTime && (
+                <Typography variant="body2" sx={{ marginBottom: '8px' }}>{checkOutTime}</Typography>
+              )}
+              <Button variant="contained" onClick={handleCheckOut} disabled={!checkInTime}>
+                Check Out
+              </Button>
+
+            </div>
+
+
+            <Typography variant="body1">
+              Work Hours: {checkInTime && checkOutTime ? calculateTotalHours(checkInTime, checkOutTime) : "10:00 hrs"}
+            </Typography>
+
+            <Button variant="contained" onClick={handleSubmit} disabled={!checkInTime || !checkOutTime}>
+              Submit
+            </Button>
+          </CardContent>
+        </Card>
+
+        <Card sx={{ mt: 2 , border: 'none'}}>
+          <CardContent sx={{ display: 'flex', justifyContent: 'flex-end', borderBottom: 'none'}}>
+            <TextField
+              label="Start Date"
+              type="date"
+              value={startMonth}
+              onChange={(e) => setStartMonth(e.target.value)}
+              sx={{ width: '150px', marginRight: '5px' }}
+            />
+            <TextField
+              label="End Date"
+              type="date"
+              value={endMonth}
+              onChange={(e) => setEndMonth(e.target.value)}
+              sx={{ width: '150px', marginRight: '5px' }}
+            />
+            <Button variant="contained" onClick={handleSearch}>
+              <SearchIcon />
+            </Button>
+          </CardContent>
+        </Card>
 
 
 
+        <Card sx={{ mt: 2 }}>
+          <CardContent>
+            {attendanceData.length > 0 && (
+              <Box mt={4}>
+
+                {/* <Table>
+                  <TableHead >
+                    <TableRow >
+                      <TableCell sx={{ fontWeight: 'bold' }}>Shift Date</TableCell>
+                      <TableCell sx={{ fontWeight: 'bold' }}>First Check-In</TableCell>
+                      <TableCell sx={{ fontWeight: 'bold' }}>Last Check-Out</TableCell>
+                      <TableCell sx={{fontWeight: 'bold' }}>Total Work Hours</TableCell>
+                      <TableCell sx={{ fontWeight: 'bold' }}>Actions</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+
+
+                    {attendanceData.map((record) => (
+                    <TableRow key={record.id}>
+                      <TableCell>{record.date}</TableCell>
+                      <TableCell>
+                        {editMode === record.id ? (
+                          <TextField
+                            value={editedCheckIn}
+                            onChange={(e) => setEditedCheckIn(e.target.value)}
+                          />
+                        ) : (
+                          record.checkIn
+                        )}
+                      </TableCell>
+                      <TableCell>
+                        {editMode === record.id ? (
+                          <TextField
+                            value={editedCheckOut}
+                            onChange={(e) => setEditedCheckOut(e.target.value)}
+                          />
+                        ) : (
+                          record.checkOut
+                        )}
+                      </TableCell>
+                      <TableCell>{record.totalHours}</TableCell>
+                      <TableCell>
+                        {editMode === record.id ? (
+                          <>
+
+                          </>
+                        ) : (
+                          <Button size="small" onClick={() => handleEdit(record.id)}>
+                            <EditIcon />
+                          </Button>
+                        )}
+                          <Button size="small" onClick={() => handleDelete(record.id)}>
+                            <DeleteIcon />
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table> */}
+
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell sx={{ fontWeight: 'bold' }}> Shift Date</TableCell>
+                      <TableCell sx={{ fontWeight: 'bold' }}>First Check-In</TableCell>
+                      <TableCell sx={{ fontWeight: 'bold' }}>Last Check-Out</TableCell>
+                      <TableCell sx={{ fontWeight: 'bold' }}>Total Hours</TableCell>
+                      <TableCell sx={{ fontWeight: 'bold' }}>Actions</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {attendanceData.map((record) => (
+                      <TableRow key={record.id}>
+                        {editMode === record.id ? (
+                          <>
+                            <TableCell>{record.date}</TableCell>
+                            <TableCell>
+                              <TextField
+                                value={editedCheckIn}
+                                onChange={(e) => setEditedCheckIn(e.target.value)}
+                              />
+                            </TableCell>
+                            <TableCell>
+                              <TextField
+                                value={editedCheckOut}
+                                onChange={(e) => setEditedCheckOut(e.target.value)}
+                              />
+                            </TableCell>
+                            <TableCell>{record.totalHours}</TableCell>
+                            <TableCell>
+                              <Button onClick={() => handleSave(record.id, editedCheckIn, editedCheckOut)}>
+                                Save
+                              </Button>
+                            </TableCell>
+                          </>
+                        ) : (
+                          <>
+                            <TableCell>{record.date}</TableCell>
+                            <TableCell>{record.checkIn}</TableCell>
+                            <TableCell>{record.checkOut}</TableCell>
+                            <TableCell>{record.totalHours}</TableCell>
+                            <TableCell>
+                              <Button onClick={() => handleEdit(record.id)} startIcon={<EditIcon />}>
+
+                              </Button>
+                              <Button onClick={() => handleDelete(record.id)} startIcon={<DeleteIcon />}>
+
+                              </Button>
+                            </TableCell>
+                          </>
+                        )}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
 
 
 
-      <Card sx={{ mt: 2 }}>
-        <CardContent sx={{ display: 'flex', justifyContent: 'flex-end', border: 'none' }}>
-
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Shift Date</TableCell>
-                <TableCell>First Check In</TableCell>
-                <TableCell>Last Check Out</TableCell>
-                <TableCell>Total Hours</TableCell>
-                <TableCell>Actions</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {attendanceData.map((record) => (
-                <TableRow key={record.id}>
-                  <TableCell>{record.date}</TableCell>
-                  <TableCell>
-                    {editMode === record.id ? (
-                      <TextField
-                        value={editedCheckIn}
-                        onChange={(e) => setEditedCheckIn(e.target.value)}
-                      />
-                    ) : (
-                      record.checkIn
-                    )}
-                  </TableCell>
-                  <TableCell>
-                    {editMode === record.id ? (
-                      <TextField
-                        value={editedCheckOut}
-                        onChange={(e) => setEditedCheckOut(e.target.value)}
-                      />
-                    ) : (
-                      record.checkOut
-                    )}
-                  </TableCell>
-                  <TableCell>{record.totalHours}</TableCell>
-                  <TableCell>
-                    {editMode === record.id ? (
-                      <Button
-                        variant="contained"
-                        color="success"
-                        startIcon={<SaveIcon />}
-                        onClick={() => handleSave(record.id)}
-                      >
-                        Save
-                      </Button>
-                    ) : (
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        startIcon={<EditIcon />}
-                        onClick={() => handleEdit(record.id, record.checkIn, record.checkOut)}
-                      >
-                        {/* Edit */}
-                      </Button>
-                    )}
-                    <Button
-                      variant="contained"
-                      color="error"
-                      startIcon={<DeleteIcon />}
-                      onClick={() => handleDelete(record.id)}
-                    >
-                      {/* Delete */}
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </CardContent>
+              </Box>
+            )}
+          </CardContent>
+        </Card>
       </Card>
-    </Card>
+    </Box>
   );
 };
 
 export default AttendancePage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
